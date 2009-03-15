8562808611fc6fc9b9489ec0b888c561fa6eb2cc
Jesse Lang <jesse@jesselang.com>
2009-03-14 23:16:08 -0500

    Get rid of the jack thread package.  Get ready to use analysis.

--- solid-audio-analysis.ads
+++ solid-audio-analysis.ads
@@ -3,13 +3,11 @@ with Ada.Numerics.Generic_Complex_Elementary_Functions;
 with Interfaces;
 
 package Solid.Audio.Analysis is
-   -- This package is the basis for transforms between the time and frequency domains.
-
-   package Complex is new Ada.Numerics.Generic_Complex_Types (Solid.Audio.Sample);
+   -- This package provides base types for transforms between the time and frequency domains.
+   package Complex   is new Ada.Numerics.Generic_Complex_Types (Solid.Audio.Sample);
+   package Functions is new Ada.Numerics.Generic_Complex_Elementary_Functions (Complex);
 
    subtype List_Index is Interfaces.Unsigned_32;
-
    type Complex_List is array (List_Index range <>) of Complex.Complex;
-
-   package Functions is new Ada.Numerics.Generic_Complex_Elementary_Functions (Complex);
+   type Complex_Handle is access Complex_List;
 end Solid.Audio.Analysis;
--- solid-audio.ads
+++ solid-audio.ads
@@ -7,15 +7,16 @@ package Solid.Audio is
    type Sample_Buffer is array (Buffer_Size range <>) of aliased Sample;
    pragma Convention (C, Sample_Buffer); -- Not sure if this is needed to pass buffers around.
    type Buffer_Handle is access Sample_Buffer;
-   package Buffers is new Interfaces.C.Pointers (Index              => Buffer_Size,
-                                                 Element            => Sample,
-                                                 Element_Array      => Sample_Buffer,
-                                                 Default_Terminator => Sample'First);
-                                                 -- Do *not* use operations that use Default_Terminator.
-                                                 -- Use length-based operations only.
-                                                 -- This is not a valid terminator.
+   package C_Buffers is new Interfaces.C.Pointers (Index              => Buffer_Size,
+                                                   Element            => Sample,
+                                                   Element_Array      => Sample_Buffer,
+                                                   Default_Terminator => Sample'First);
+                                                   -- Do *not* use operations that use Default_Terminator.
+                                                   -- Use length-based operations only.
+                                                   -- This is not a valid terminator.
 
-   subtype Buffer_Pointer is Buffers.Pointer;    -- This is an access type for Sample.
+   subtype Buffer_Pointer is C_Buffers.Pointer; -- This is an access type for Sample.
+   subtype Sample_Handle  is C_Buffers.Pointer;
 
    type Sample_Rate is range 8000 .. 48000;
 end Solid.Audio;
