e51693d7eb9ac0bc69260b8d9667cba3f3ff206f
Jesse Lang <jesse@jesselang.com>
2009-08-30 01:25:55 -0500

    Move the audio packages to their own directory.  Preparing to move audio packages to its own repo.

--- solid-audio-jack-thread.ads
+++
@@ -1,6 +0,0 @@
-with Interfaces.C.Extensions;
-
-private package Solid.Audio.Jack.Thread is
-   procedure Initialize (Argument : in Interfaces.C.Extensions.void_ptr);
-   pragma Convention (C, Initialize);
-end Solid.Audio.Jack.Thread;
--- solid-audio-ladspa.ads
+++
@@ -1,8 +0,0 @@
-package Solid.Audio.Ladspa is
-   type Plugin_Index is new Interfaces.C.unsigned_long;
-   type Plugin_ID is new Interfaces.C.unsigned_long;
-
-   type Plugin_Handle is new Interfaces.C.Extensions.opaque_structure_def_ptr;
-
-   subtype Control_Value is Sample; -- This is required for connecting audio ports.  Otherwise GNAT gives a bug box.
-end Solid.Audio.Ladspa;
